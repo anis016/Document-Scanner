a021e97fa9b14ab798816a24d61413c6823cf798
anis016 <anis.cuet016@gmail.com>
2018-05-19 18:47:11 +0200

    convert image into grayscale and load the face cascade detector

--- cv_api/face_detector/views.py
+++ cv_api/face_detector/views.py
@@ -1,11 +1,17 @@
-from django.shortcuts import render
 # import the necessary packages
+
+from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 import numpy as np
 import urllib
 import json
 import cv2
+import os
+
+# define the path to the face detector
+FACE_DETECTOR_PATH = "{base_path}/cascades/haarcascade_frontalface_default.xml".format(
+    base_path=os.path.abspath(os.path.dirname(__file__)))
 
 @csrf_exempt
 def detect(request):
@@ -32,10 +38,17 @@ def detect(request):
             # load the image and convert
             image = _grab_image(url=url)
 
-        ### START WRAPPING OF COMPUTER VISION APP
-        # Insert code here to process the image and update
-        # the `data` dictionary with your results
-        ### END WRAPPING OF COMPUTER VISION APP
+        # convert the image to gray-scale, load the face cascade detector,
+        # and detect the faces in the image.
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        detector = cv2.CascadeClassifier(FACE_DETECTOR_PATH)
+        rects = detector.detectMultiScale(image, scalefactor=1.1, minNeighbors=5, minSize=(30, 30),
+                                          flags=cv2.cv.cv_HAAR_SCALE_IMAGE)
+        # construct a list of bounding boxes from the detection
+        rects = [(int(x), int(y), int(x+w), int(y+h)) for (x, y, w, h) in rects]
+
+        # update the data dictionary with the faces detected
+        data.update({"num_faces": len(rects), "faces": rects, "success": True})
 
         # update the data dictionary
         data["success"] = True
